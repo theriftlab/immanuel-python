b326f5034135f6caefd7141311a814a50e2fd731
Robert Davies <robert@theriftlab.com>
2022-06-26 22:24:45 -0700

    Variable rename.

--- immanuel/tools/pattern.py
+++ immanuel/tools/pattern.py
@@ -16,40 +16,40 @@ from immanuel.const import chart, calc
 
 def chart_shape(items: dict) -> int:
     longitudes = sorted([v['lon'] for v in items.values() if v['type'] == chart.PLANET])
-    gaps = [swe.difdegn(_next(longitudes, k), v) for k, v in enumerate(longitudes)]
-    max_gap = max(gaps)
+    diffs = [swe.difdegn(_next(longitudes, k), v) for k, v in enumerate(longitudes)]
+    max_diff = max(diffs)
 
     # All planets within 120º can only be a bundle
-    if max_gap >= 240-options.chart_shape_orb:
+    if max_diff >= 240-options.chart_shape_orb:
         return calc.BUNDLE
 
     # Bucket handle planet(s) must be at least 90º from edges of main cluster
-    for k, v in enumerate(gaps):
-        next = _next(gaps, k)
-        second_next = _next(gaps, k, 2)
+    for k, v in enumerate(diffs):
+        next = _next(diffs, k)
+        second_next = _next(diffs, k, 2)
 
         if v >= 90-options.chart_shape_orb and (next >= 90-options.chart_shape_orb or (next <= options.chart_shape_orb and second_next >= 90-options.chart_shape_orb)):
             return calc.BUCKET
 
     # All planets being within 180º with no bucket handle means a bowl
-    if max_gap >= 180-options.chart_shape_orb:
+    if max_diff >= 180-options.chart_shape_orb:
         return calc.BOWL
 
     # All planets being within 240º with no bucket handle means a locomotive
-    if max_gap >= 120-options.chart_shape_orb:
+    if max_diff >= 120-options.chart_shape_orb:
         return calc.LOCOMOTIVE
 
-    gaps.sort()
+    diffs.sort()
 
     # Only two gaps of at least 60º mean a seesaw
-    if len([v for v in gaps if v >= 60-options.chart_shape_orb]) == 2:
+    if len([v for v in diffs if v >= 60-options.chart_shape_orb]) == 2:
         return calc.SEESAW
 
     # Three gaps of at least 30º mean a splay
-    if len([v for v in gaps if v >= 30-options.chart_shape_orb]) == 3:
+    if len([v for v in diffs if v >= 30-options.chart_shape_orb]) == 3:
         return calc.SPLAY
 
-    # Default to no particular patten
+    # Default to no particular pattern
     return calc.SPLASH
 
 
