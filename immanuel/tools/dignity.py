b22a137472a1106573c080bbab745382ef0e270d
Robert Davies <robert@theriftlab.com>
2022-08-14 18:15:41 -0700

    Fixes / tidyups in dignities module.

--- immanuel/tools/dignity.py
+++ immanuel/tools/dignity.py
@@ -46,10 +46,10 @@ def mutual_reception_exaltaion(item: dict, items: dict) -> bool:
 def mutual_reception_house(item: dict, items: dict, houses: dict) -> bool:
     """ Whether the passed planet is in mutual reception by
     house-sign rulership. """
-    house = houses[position.house(item['lon'], houses)]
+    house = houses[position.house(item['lon'], houses)['index']]
     house_sign = position.sign(house['lon'])
     house_sign_ruler = items[options.rulerships[house_sign]]
-    house_sign_ruler_house = houses[position.house(house_sign_ruler['lon'], houses)]
+    house_sign_ruler_house = houses[position.house(house_sign_ruler['lon'], houses)['index']]
     house_sign_ruler_house_sign = position.sign(house_sign_ruler_house['lon'])
 
     return item['index'] == options.rulerships[house_sign_ruler_house_sign]
@@ -119,56 +119,40 @@ def all(item: dict, **kwargs) -> dict:
     houses = kwargs.get('houses', None)
     is_daytime = kwargs.get('is_daytime', None)
 
-    dignity_state = {}
-
-    if dignities.RULER in options.dignity_scores:
-        dignity_state[dignities.RULER] = ruler(item)
-
-    if dignities.EXALTED in options.dignity_scores:
-        dignity_state[dignities.RULER] = exalted(item)
-
-    if dignities.TERM_RULER in options.dignity_scores:
-        dignity_state[dignities.TERM_RULER] = term_ruler(item)
-
-    if dignities.FACE_RULER in options.dignity_scores:
-        dignity_state[dignities.FACE_RULER] = face_ruler(item)
+    dignity_state = {
+        dignities.RULER: ruler(item) if dignities.RULER in options.dignity_scores else None,
+        dignities.EXALTED: exalted(item) if dignities.EXALTED in options.dignity_scores else None,
+        dignities.TERM_RULER: term_ruler(item) if dignities.TERM_RULER in options.dignity_scores else None,
+        dignities.FACE_RULER: face_ruler(item) if dignities.FACE_RULER in options.dignity_scores else None,
+    }
 
     if is_daytime is not None:
-        if dignities.TRIPLICITY_RULER_DAY in options.dignity_scores:
-            dignity_state[dignities.TRIPLICITY_RULER_DAY] = triplicity_ruler_day(item, is_daytime)
-
-        if dignities.TRIPLICITY_RULER_NIGHT in options.dignity_scores:
-            dignity_state[dignities.TRIPLICITY_RULER_NIGHT] = triplicity_ruler_night(item, is_daytime)
-
-        if dignities.TRIPLICITY_RULER_PARTICIPATORY in options.dignity_scores:
-            dignity_state[dignities.TRIPLICITY_RULER_PARTICIPATORY] = triplicity_ruler_participatory(item)
+        dignity_state |= {
+            dignities.TRIPLICITY_RULER_DAY: triplicity_ruler_day(item, is_daytime) if dignities.TRIPLICITY_RULER_DAY in options.dignity_scores else None,
+            dignities.TRIPLICITY_RULER_NIGHT: triplicity_ruler_night(item, is_daytime) if dignities.TRIPLICITY_RULER_NIGHT in options.dignity_scores else None,
+            dignities.TRIPLICITY_RULER_PARTICIPATORY: triplicity_ruler_participatory(item) if dignities.TRIPLICITY_RULER_PARTICIPATORY in options.dignity_scores else None,
+        }
 
     if options.peregrine == dignities.IGNORE_MUTUAL_RECEPTIONS:
         peregrine = len([v for v in dignity_state.values() if v]) == 0
 
     if items is not None:
-        if dignities.MUTUAL_RECEPTION_RULERSHIP in options.dignity_scores:
-            dignity_state[dignities.MUTUAL_RECEPTION_RULERSHIP] = mutual_reception_rulership(item, items)
-
-        if dignities.MUTUAL_RECEPTION_EXALTATION in options.dignity_scores:
-            dignity_state[dignities.MUTUAL_RECEPTION_EXALTATION] = mutual_reception_exaltaion(item, items)
-
-        if houses is not None and dignities.MUTUAL_RECEPTION_HOUSE in options.dignity_scores:
-            dignity_state[dignities.MUTUAL_RECEPTION_HOUSE] = mutual_reception_house(item, items, houses)
+        dignity_state |= {
+            dignities.MUTUAL_RECEPTION_RULERSHIP: mutual_reception_rulership(item, items) if dignities.MUTUAL_RECEPTION_RULERSHIP in options.dignity_scores else None,
+            dignities.MUTUAL_RECEPTION_EXALTATION: mutual_reception_exaltaion(item, items) if dignities.MUTUAL_RECEPTION_EXALTATION in options.dignity_scores else None,
+            dignities.MUTUAL_RECEPTION_HOUSE: mutual_reception_house(item, items, houses) if houses is not None and dignities.MUTUAL_RECEPTION_HOUSE in options.dignity_scores else None,
+        }
 
     if options.peregrine == dignities.INCLUDE_MUTUAL_RECEPTIONS:
         peregrine = len([v for v in dignity_state.values() if v]) == 0
 
-    if dignities.DETRIMENT in options.dignity_scores:
-        dignity_state[dignities.DETRIMENT] = detriment(item)
+    dignity_state |= {
+        dignities.DETRIMENT: detriment(item) if dignities.DETRIMENT in options.dignity_scores else None,
+        dignities.FALL: fall(item) if dignities.FALL in options.dignity_scores else None,
+        dignities.PEREGRINE: peregrine if dignities.PEREGRINE in options.dignity_scores else None,
+    }
 
-    if dignities.FALL in options.dignity_scores:
-        dignity_state[dignities.FALL] = fall(item)
-
-    if dignities.PEREGRINE in options.dignity_scores:
-        dignity_state[dignities.PEREGRINE] = peregrine
-
-    return dignity_state
+    return {k: v for k, v in dignity_state.items() if v is not None}
 
 
 def score(item: dict, **kwargs) -> int:
@@ -177,6 +161,6 @@ def score(item: dict, **kwargs) -> int:
     return sum([v for k, v in options.dignity_scores.items() if k in dignity_state and dignity_state[k]])
 
 
-def _planet_signs(item: dict, table: dict) -> int:
+def _planet_signs(item: dict, table: dict) -> tuple:
     """ Returns the sign(s) a planet belongs to in {sign: planet} dicts. """
     return tuple(k for k, v in table.items() if v == item['index'])
