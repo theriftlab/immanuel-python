f5f2f4adef21ae49150c7f09f626f6e201e2ef2d
Robert Davies <robert@theriftlab.com>
2022-08-22 17:51:06 -0700

    Refactored date module.

--- immanuel/tools/date.py
+++ immanuel/tools/date.py
@@ -14,9 +14,10 @@
 """
 
 from datetime import datetime
+from dateutil import tz
+from zoneinfo import ZoneInfo
 
 import swisseph as swe
-from pytz import exceptions, timezone, UTC
 from timezonefinder import TimezoneFinder
 
 
@@ -24,29 +25,27 @@ class DateTime:
     """ This class is instatiated with either a standard Python datetime
     object or a Julian day as a float, and decimal lat / lon coordinates
     as floats. is_dst can be True or False to clarify ambiguous datetimes
-    (eg. 01:30 when DST ends).
+    (eg. 01:30 when DST ends). If is_dst omitted, datetime's fold is used.
 
     """
 
     def __init__(self, dt_jd: datetime | float, lat: float, lon: float, is_dst: bool = None) -> None:
         self.datetime = None
+        self.jd = None
+        self.timezone = TimezoneFinder().timezone_at(lat=lat, lng=lon)
         self.lat = lat
         self.lon = lon
-        self.timezone = timezone(TimezoneFinder().certain_timezone_at(lat=lat, lng=lon))
-        self.dst_ambiguous = None
-        self.jd = None
 
         if isinstance(dt_jd, datetime):
-            try:
-                self.datetime = self.timezone.localize(dt_jd, is_dst) if dt_jd.tzinfo is None else dt_jd.astimezone(self.timezone)
-                self.jd = datetime_to_jd(self.datetime)
-                self.dst_ambiguous = False
-            except exceptions.AmbiguousTimeError:
-                self.dst_ambiguous = True
+            self.datetime = dt_jd.replace(tzinfo=ZoneInfo(self.timezone), fold=1 if is_dst == False else 0)
+            self.jd = datetime_to_jd(self.datetime)
         else:
+            self.datetime = jd_to_datetime(dt_jd).astimezone(ZoneInfo(self.timezone)).replace(fold=1 if is_dst == False else 0)
             self.jd = dt_jd
-            self.datetime = jd_to_datetime(dt_jd).astimezone(self.timezone)
-            self.dst_ambiguous = False
+
+    def ambiguous(self) -> bool:
+        """ Check whether this is an ambiguous DST transition. """
+        return tz.datetime_ambiguous(self.datetime)
 
     def isoformat(self) -> str:
         """ Returns the underlying datetime object's ISO format. """
@@ -54,16 +53,13 @@ class DateTime:
 
     def __str__(self) -> str:
         """ Returns the full date with timezone string. """
-        if self.dst_ambiguous:
-            return 'Ambiguous Time Error'
-
-        return f'{self.datetime.strftime("%a %d %b %Y %H:%M:%S")} {self.timezone.zone}'
+        return f'{self.datetime.strftime("%a %d %b %Y %H:%M:%S")} {self.timezone}'
 
 
 def datetime_to_jd(dt: datetime) -> float:
     """ Convert localised datetime into universal Julian day. """
-    utc_dt = dt.datetime.astimezone(UTC) if isinstance(dt, DateTime) else dt.astimezone(UTC)
-    return swe.utc_to_jd(utc_dt.year, utc_dt.month, utc_dt.day, utc_dt.hour, utc_dt.minute, utc_dt.second)[1]
+    utc_dt = dt.datetime.astimezone(ZoneInfo('UTC')) if isinstance(dt, DateTime) else dt.astimezone(ZoneInfo('UTC'))
+    return swe.utc_to_jd(*utc_dt.timetuple()[0:6])[1]
 
 
 def jd_to_datetime(jd: float) -> datetime:
@@ -73,4 +69,4 @@ def jd_to_datetime(jd: float) -> datetime:
     seconds = int(seconds_float)
     microseconds = round((seconds_float - seconds) * 1000000)
     dt_utc = swe_utc[:5] + (seconds, microseconds)
-    return datetime(*dt_utc, tzinfo=UTC)
+    return datetime(*dt_utc, tzinfo=ZoneInfo('UTC'))
