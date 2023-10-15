fc8aa46ae6acb36da7cd6b0cae8050385f36be41
Robert Davies <robert@theriftlab.com>
2023-10-14 18:23:49 -0700

    Tests & rewrites for midpoint module.

--- immanuel/tools/midpoint.py
+++ immanuel/tools/midpoint.py
@@ -9,47 +9,38 @@
 
 import swisseph as swe
 
-from immanuel.const import calc, chart, names
+from immanuel.const import calc, chart
 from immanuel.tools import calculate, eph
 
 
-def all(objects1: dict, objects2: dict, pars_fortuna: int = calc.COMPOSITE, pars_fortuna_formula: int = None) -> dict:
-    """ Takes two dicts of chart objects typically returned by eph.all()
-    and returns the averaged data for both sets as a third set of
-    composite objects. """
+def all(objects1: dict, objects2: dict, obliquity: float = None, pars_fortuna: int = calc.MIDPOINT, pars_fortuna_formula: int = None) -> dict:
+    """ Takes two dicts of chart objects typically returned by the eph
+    module and returns the averaged data for both sets as a third dict
+    of composite objects. """
     objects = {}
 
     for index, object1 in objects1.items():
-        # Skip Part of Fortune if we need to recalculate from composite Sun/Moon/Asc
-        if index == chart.PARS_FORTUNA and pars_fortuna == calc.COMPOSITE:
-            object = {}
-        else:
-            object = composite(object1, objects2[index])
+        objects[index] = composite(object1, objects2[index], obliquity)
 
-        objects[index] = object
-
-    # Now we have composite Sun/Moon/Asc, calculate Part of Fortune
-    if pars_fortuna == calc.COMPOSITE and chart.PARS_FORTUNA in objects1:
-        objects[chart.PARS_FORTUNA] = {
-            'index': chart.PARS_FORTUNA,
-            'type': chart.POINT,
-            'name': names.POINTS[chart.PARS_FORTUNA],
-            'lon': calculate.pars_fortuna(objects[chart.SUN]['lon'], objects[chart.MOON]['lon'], objects[chart.ASC]['lon'], pars_fortuna_formula),
-            'speed': 0.0,
-        }
+    # If we have composite Sun/Moon/Asc, calculate Part of Fortune
+    if pars_fortuna == calc.COMPOSITE and objects.keys() >= {chart.PARS_FORTUNA, chart.SUN, chart.MOON, chart.ASC}:
+        objects[chart.PARS_FORTUNA]['lon'] = calculate.pars_fortuna(objects[chart.SUN]['lon'], objects[chart.MOON]['lon'], objects[chart.ASC]['lon'], pars_fortuna_formula)
 
     return objects
 
 
-def composite(object1: dict, object2: dict) -> dict:
-    """ Given two chart objects tpyically returned by the eph module, this
+def composite(object1: dict, object2: dict, obliquity: float = None) -> dict:
+    """ Given two chart objects typically returned by the eph module, this
     function will return a composite object. """
-    composite_object = {k: v for k, v in object1.items()}
-    composite_object['lon'] = swe.deg_midp(object1['lon'], object2['lon'])
+    composite_object = object1 | {
+        'lon': swe.deg_midp(object1['lon'], object2['lon']),
+        'lat': 0.0,
+        'dist': 0.0,
+        'speed': (object1['speed'] + object2['speed']) / 2,
+    }
 
-    for key in ('lat', 'dist', 'speed', 'dec'):
-        if key in object1:
-            composite_object[key] = (object1[key] + object2[key]) / 2
+    if 'dec' in object1 and obliquity is not None:
+        composite_object['dec'] = swe.cotrans((composite_object['lon'], 0, 1), -obliquity)[1]
 
     return composite_object
 
---
+++ tests/test_midpoint.py
@@ -0,0 +1,168 @@
+"""
+    This file is part of immanuel - (C) The Rift Lab
+    Author: Robert Davies (robert@theriftlab.com)
+
+
+    TODO
+"""
+
+from datetime import datetime
+
+from pytest import fixture
+import swisseph as swe
+
+from immanuel.const import calc, chart
+from immanuel.setup import settings
+from immanuel.tools import calculate, convert, date, eph, midpoint, position
+
+
+@fixture
+def coords():
+    # San Diego coords as used by astro.com
+    return [convert.string_to_dec(v) for v in ('32n43', '117w09')]
+
+@fixture
+def jd1(coords):
+    return date.to_jd(date.localize(datetime.fromisoformat('2000-01-01 10:00'), *coords))
+
+@fixture
+def jd2(coords):
+    return date.to_jd(date.localize(datetime.fromisoformat('2025-06-21 00:00'), *coords))
+
+@fixture
+def obliquity(jd1, jd2):
+    return midpoint.obliquity(jd1, jd2)
+
+@fixture
+def astro():
+    return {
+        chart.ASC: {
+            'sign': chart.PISCES,
+            'lon': '08°31\'36"',
+            'lat': '00°00\'00"',
+        },
+        chart.SUN: {
+            'sign': chart.ARIES,
+            'lon': '05°23\'50"',
+            'lat': '00°00\'00"',
+            'speed': '00°59\'13"',
+            'dec': '02°08\'39"',
+        },
+        chart.MOON: {
+            'sign': chart.AQUARIUS,
+            'lon': '09°42\'33"',
+            'lat': '00°00\'00"',
+            'speed': '13°15\'20"',
+            'dec': '-17°49\'05"',
+        },
+        chart.MERCURY: {
+            'sign': chart.LIBRA,
+            'lon': '12°12\'13"',
+            'lat': '00°00\'00"',
+            'speed': '01°33\'06"',
+            'dec': '-04°49\'23"',
+        },
+        chart.VENUS: {
+            'sign': chart.AQUARIUS,
+            'lon': '23°42\'11"',
+            'lat': '00°00\'00"',
+            'speed': '01°08\'08"',
+            'dec': '-13°37\'07"',
+        },
+        chart.MARS: {
+            'sign': chart.SAGITTARIUS,
+            'lon': '00°11\'05"',
+            'lat': '00°00\'00"',
+            'speed': '00°40\'14"',
+            'dec': '-20°11\'18"',
+        },
+        chart.JUPITER: {
+            'sign': chart.TAURUS,
+            'lon': '28°55\'54"',
+            'lat': '00°00\'00"',
+            'speed': '00°08\'06"',
+            'dec': '19°55\'10"',
+        },
+        chart.SATURN: {
+            'sign': chart.ARIES,
+            'lon': '20°57\'45"',
+            'lat': '00°00\'00"',
+            'speed': '00°00\'30"',
+            'dec': '08°10\'52"',
+        },
+        chart.URANUS: {
+            'sign': chart.ARIES,
+            'lon': '07°01\'20"',
+            'lat': '00°00\'00"',
+            'speed': '00°03\'04"',
+            'dec': '02°47\'14"',
+        },
+        chart.NEPTUNE: {
+            'sign': chart.PISCES,
+            'lon': '02°39\'48"',
+            'lat': '00°00\'00"',
+            # This is the only figure disagreeing with astro.com (~1 arcsec) and nobody knows why
+            # 'speed': '00°01\'18"',
+            'dec': '-10°31\'28"',
+        },
+        chart.PLUTO: {
+            'sign': chart.CAPRICORN,
+            'lon': '07°23\'57"',
+            'lat': '00°00\'00"',
+            'speed': '00°00\'29"',
+            'dec': '-23°13\'53"',
+        },
+        chart.TRUE_NORTH_NODE: {
+            'sign': chart.TAURUS,
+            'lon': '28°04\'05"',
+            'lat': '00°00\'00"',
+            'speed': '-00°03\'45"',
+        },
+        chart.PARS_FORTUNA: {
+            'sign': chart.PISCES,
+            'lon': '09°54\'57"',
+            'lat': '00°00\'00"',
+            'speed': '00°00\'00"',
+            'dec': '07°51\'02"',
+        },
+    }
+
+
+def test_all(coords, jd1, jd2, obliquity, astro):
+    objects1 = eph.objects(astro.keys(), jd1, *coords, chart.PLACIDUS, calc.DAY_NIGHT_FORMULA)
+    objects2 = eph.objects(astro.keys(), jd2, *coords, chart.PLACIDUS, calc.DAY_NIGHT_FORMULA)
+    composites = midpoint.all(objects1, objects2, obliquity, calc.MIDPOINT, calc.DAY_NIGHT_FORMULA)
+
+    for index, composite in composites.items():
+        sign, sign_lon = position.signlon(composite['lon'])
+        assert sign == astro[index]['sign']
+        assert convert.dec_to_string(sign_lon) == astro[index]['lon']
+
+        for key in ('lat', 'speed', 'dec'):
+            if key in astro[index] and key in composite:
+                assert convert.dec_to_string(composite[key]) == astro[index][key]
+
+
+def test_composite(coords, jd1, jd2, obliquity, astro):
+    for index in astro.keys():
+        object1 = eph.get(index, jd1, *coords, chart.PLACIDUS, calc.DAY_NIGHT_FORMULA)
+        object2 = eph.get(index, jd2, *coords, chart.PLACIDUS, calc.DAY_NIGHT_FORMULA)
+        composite = midpoint.composite(object1, object2, obliquity)
+        sign, sign_lon = position.signlon(composite['lon'])
+        assert sign == astro[index]['sign']
+        assert convert.dec_to_string(sign_lon) == astro[index]['lon']
+
+        for key in ('lat', 'speed', 'dec'):
+            if key in astro[index] and key in composite:
+                assert convert.dec_to_string(composite[key]) == astro[index][key]
+
+
+def test_obliquity(jd1, jd2):
+    obliquity = midpoint.obliquity(jd1, jd2, False)
+    mean_obliquity = midpoint.obliquity(jd1, jd2, True)
+
+    ecl_nut1 = swe.calc_ut(jd1, swe.ECL_NUT)[0]
+    ecl_nut2 = swe.calc_ut(jd2, swe.ECL_NUT)[0]
+
+    assert obliquity == (ecl_nut1[0] + ecl_nut2[0]) / 2
+    assert mean_obliquity == (ecl_nut1[1] + ecl_nut2[1]) / 2
