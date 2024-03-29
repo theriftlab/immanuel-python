69ae6a1a8169a881a6e6c2007541b8826a8611f3
Robert Davies <robert@theriftlab.com>
2024-03-28 18:28:40 -0700

    Support parent locales for translation files.

--- immanuel/classes/localize.py
+++ immanuel/classes/localize.py
@@ -3,11 +3,14 @@
     Author: Robert Davies (robert@theriftlab.com)
 
 
-    Set up translations.
+    Sets up translations and provides our own _() function. This will look for
+    a translation file for the full locale and fall back to the parent locale,
+    for example pt_BR then pt. If a file is found, then the full locale string
+    (eg. pt_BR) will be passed to locale.setlocale() for localizing datetimes.
 
 """
 
-import gettext, locale, os
+import gettext, locale
 from pathlib import Path
 
 from immanuel.setup import settings
@@ -16,15 +19,15 @@ from immanuel.setup import settings
 class Localize:
     translation = None
 
-    def get_translation() -> gettext.NullTranslations:
+    def get_translation() -> gettext.NullTranslations | gettext.GNUTranslations:
         if Localize.translation is None:
             localedir = (Path(__file__).parent.parent.parent / 'locales').absolute()
-            languages = [settings.locale]
+            languages = [settings.locale, settings.locale[:2]]
 
             Localize.translation = gettext.translation('immanuel', localedir=localedir, languages=languages, fallback=True)
 
-            if os.path.exists(localedir / settings.locale):
-                locale.setlocale(locale.LC_TIME, '' if settings.locale is None else settings.locale)
+            if isinstance(Localize.translation, gettext.GNUTranslations):
+                locale.setlocale(locale.LC_TIME, settings.locale)
 
         return Localize.translation
 
